ded2fb01dd5e42d57c60c24f53956a98ba4aeee9
Heinstein <vonsolmsheino@gmail.com>
2024-08-15 11:07:27 +0200

    Added new Office Props asset

--- Assets/ScifiOfficeLite/Scripts/DemoFirstPersonController.cs
+++ Assets/ScifiOfficeLite/Scripts/DemoFirstPersonController.cs
@@ -1,6 +1,8 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-namespace ScifiOfficeLite.Scripts {
+namespace ScifiOffice {
     public class DemoFirstPersonController : MonoBehaviour {
 
         Rigidbody rb;
